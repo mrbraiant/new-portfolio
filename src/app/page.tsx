0868615ae1f99f700679ec031c9fb1906a37c9e9
mrbraiant <mrbraiant@gmail.com>
2024-05-04 14:34:32 +0100

    build: creating components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+import { MiniDrawer } from './components/drawer';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <div>
         <h1>Hello World</h1>
+        <MiniDrawer />
       </div>
     </main>
   );
