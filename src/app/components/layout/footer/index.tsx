ffd86f3a995cf06fbc663b97c19e8948e7913361
mrbraiant <mrbraiant@gmail.com>
2024-05-22 18:56:30 +0100

    fix: mobile footer fixed

--- src/app/components/layout/footer/index.tsx
+++ src/app/components/layout/footer/index.tsx
@@ -7,111 +7,109 @@ import {
   WhatsApp,
 } from '@mui/icons-material';
 import { PalletColor } from '@utils/palletColor';
-import { SocialMediaBox } from './styles';
+import {
+  FooterBox,
+  FooterTopContent,
+  LogoBox,
+  SocialMediaBox,
+} from './styles';
 import { Logo } from '../images/logo';
 import { StyledLink } from '@components/styledLink';
 
 export const Footer = () => {
   return (
     <footer>
-      <div
-        style={{
-          backgroundColor: PalletColor.darkMidnight,
-          height: 'fit-content',
-          padding: '1rem 1.5rem',
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        <div style={{ alignSelf: 'center' }}>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              gap: '0.5rem',
-            }}
-          >
-            <Logo />
-            <Text style={{ color: PalletColor.auraWhite }}>
-              <strong>Braiant Dev ®</strong>
-            </Text>
+      <FooterBox>
+        <FooterTopContent>
+          <div style={{ alignSelf: 'center' }}>
+            <LogoBox>
+              <Logo />
+              <Text
+                style={{ color: PalletColor.auraWhite }}
+              >
+                <strong>Braiant Dev ®</strong>
+              </Text>
+            </LogoBox>
           </div>
-          <Text
-            type="caption"
-            style={{ color: PalletColor.auraWhite }}
-          >
-            Developed with much &#9749; and ♥
-          </Text>
-        </div>
 
-        <SocialMediaBox>
-          <ContactBox>
-            <Email
-              sx={{
-                backgroundColor: PalletColor.gmail.main,
-                color: PalletColor.gmail.white,
-                borderRadius: '5px',
-              }}
-            />
+          <SocialMediaBox>
+            <ContactBox>
+              <Email
+                sx={{
+                  backgroundColor: PalletColor.gmail.main,
+                  color: PalletColor.gmail.white,
+                  borderRadius: '5px',
+                }}
+              />
 
-            <Text>
-              <StyledLink href="mailto:mrbraiant@gmail.com">
-                mrbraiant@gmail.com
+              <Text>
+                <StyledLink href="mailto:mrbraiant@gmail.com">
+                  mrbraiant@gmail.com
+                </StyledLink>
+              </Text>
+            </ContactBox>
+
+            <ContactBox>
+              <LinkedIn
+                sx={{ color: PalletColor.linkedIn.main }}
+              />
+
+              <StyledLink
+                href={
+                  'https://www.linkedin.com/in/braiant-malta-924529210/'
+                }
+                target="_blank"
+              >
+                @braiant-malta
               </StyledLink>
-            </Text>
-          </ContactBox>
+            </ContactBox>
 
-          <ContactBox>
-            <LinkedIn
-              sx={{ color: PalletColor.linkedIn.main }}
-            />
+            <ContactBox>
+              <GitHub
+                sx={{
+                  color: '#333',
+                  borderRadius: '25px',
+                }}
+              />
 
-            <StyledLink
-              href={
-                'https://www.linkedin.com/in/braiant-malta-924529210/'
-              }
-              target="_blank"
-            >
-              @braiant-malta
-            </StyledLink>
-          </ContactBox>
+              <StyledLink
+                href={'https://github.com/mrbraiant'}
+                target="_blank"
+              >
+                @mrbraiant
+              </StyledLink>
+            </ContactBox>
 
-          <ContactBox>
-            <GitHub
-              sx={{
-                color: '#333',
-                borderRadius: '25px',
-              }}
-            />
+            <ContactBox>
+              <WhatsApp
+                sx={{
+                  backgroundColor:
+                    PalletColor.whatsaApp.main,
+                  color: PalletColor.whatsaApp.white,
+                  borderRadius: '25px',
+                }}
+              />
 
-            <StyledLink
-              href={'https://github.com/mrbraiant'}
-              target="_blank"
-            >
-              @mrbraiant
-            </StyledLink>
-          </ContactBox>
+              <StyledLink
+                href={
+                  "https://wa.me/3530877753787?text=I'm%20interested%20in%20speak%20with%20you%20about%20a%20job%20offer"
+                }
+                target="_blank"
+              >
+                Text me
+              </StyledLink>
+            </ContactBox>
+          </SocialMediaBox>
+        </FooterTopContent>
 
-          <ContactBox>
-            <WhatsApp
-              sx={{
-                backgroundColor: PalletColor.whatsaApp.main,
-                color: PalletColor.whatsaApp.white,
-                borderRadius: '25px',
-              }}
-            />
-
-            <StyledLink
-              href={
-                "https://wa.me/3530877753787?text=I'm%20interested%20in%20speak%20with%20you%20about%20a%20job%20offer"
-              }
-              target="_blank"
-            >
-              Text me
-            </StyledLink>
-          </ContactBox>
-        </SocialMediaBox>
-      </div>
+        <Text
+          type="caption"
+          textAlign="center"
+          style={{ color: PalletColor.auraWhite }}
+        >
+          Developed with much &#9749; and ♥
+        </Text>
+      </FooterBox>
     </footer>
   );
 };
--- src/app/components/layout/footer/styles.ts
+++ src/app/components/layout/footer/styles.ts
@@ -1,6 +1,36 @@
 import { styled } from '@mui/material';
 import { PalletColor } from '@utils/palletColor';
 
+export const FooterBox = styled('div')(({ theme }) => ({
+  backgroundColor: PalletColor.darkMidnight,
+  height: 'fit-content',
+  padding: '1rem 1.5rem',
+  display: 'flex',
+  flexDirection: 'column',
+  // justifyContent: 'space-between',
+  [theme.breakpoints.down('sm')]: {
+    //   flexDirection: 'column',
+    gap: '1rem',
+  },
+}));
+
+export const FooterTopContent = styled('div')(
+  ({ theme }) => ({
+    display: 'flex',
+    justifyContent: 'space-between',
+    [theme.breakpoints.down('sm')]: {
+      flexDirection: 'column',
+      gap: '1rem',
+    },
+  }),
+);
+
+export const LogoBox = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '0.5rem',
+});
+
 export const SocialMediaBox = styled('div')({
   backgroundColor: '#35536f',
   padding: '0.5rem',
