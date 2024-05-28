1251ed30ea0e5667a157d219aae3cfadc08e858c
mrbraiant <mrbraiant@gmail.com>
2024-05-28 01:53:40 +0100

    build: improving skills effect and code block

--- src/app/components/styledLink/index.tsx
+++ src/app/components/styledLink/index.tsx
@@ -2,6 +2,7 @@ import { styled } from '@mui/material';
 import Link from 'next/link';
 
 export const StyledLink = styled(Link)({
+  color: 'darkblue',
   ['&:hover']: {
     textDecoration: 'underline',
   },
